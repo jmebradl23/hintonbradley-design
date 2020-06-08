4553573a8efc3f44bcb444bab13d1b93f8190e92
hintonbradley <hintonbradley@gmail.com>
2020-06-08 05:32:12 -0700

    transferred site

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import jump from 'jump.js';
+import sal from 'sal.js';
+import './style.scss';
+import '../node_modules/sal.js/dist/sal.css';
+
+$(document).ready(function() {
+    const links = document.querySelectorAll('.nav-links a');
+    links.forEach(link => link.addEventListener('click', smoothScroll));
+    sal();
+    const hamburger = document.getElementsByClassName('hamburger')[0];
+    hamburger.addEventListener('click', toggleNavbar);
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.addEventListener('click', toggleNavbar);
+});
+
+function smoothScroll (e) {
+    e.preventDefault();
+    const id = e.currentTarget.getAttribute('href');
+    jump(id, {
+        duration: 1000,
+        offset: -58
+      })
+}
+
+function toggleNavbar (e) {
+    if (window.innerWidth<776) {
+        const links = document.querySelector('.nav-links');
+        links.classList.toggle('show')
+    }
+}
