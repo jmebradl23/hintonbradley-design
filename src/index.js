4fc77d027acddf75fa8816406ff5eff50f4c0161
hintonbradley <hintonbradley@gmail.com>
2020-08-20 18:22:29 -0700

    updated js

--- src/index.js
+++ src/index.js
@@ -7,24 +7,43 @@ $(document).ready(function() {
     const links = document.querySelectorAll('.nav-links a');
     links.forEach(link => link.addEventListener('click', smoothScroll));
     sal();
-    const hamburger = document.getElementsByClassName('hamburger')[0];
+    const hamburger = document.querySelector('.hamburger');
     hamburger.addEventListener('click', toggleNavbar);
     const navLinks = document.querySelector('.nav-links');
     navLinks.addEventListener('click', toggleNavbar);
+    const body = document.getElementsByTagName('body')[0];
+    body.addEventListener('click', checkForSmallNav);
 });
+
+function checkForSmallNav(e) {
+    const links = document.querySelector('.nav-links');
+    if(links.classList.contains('show')){
+        e.stopPropagation();
+        e.preventDefault();
+        links.classList.remove('show');
+    }
+    return false;
+}
 
 function smoothScroll (e) {
     e.preventDefault();
+    e.stopPropagation();
+    let links = document.querySelector('.nav-links');
+    if (links.classList.contains('show')){toggleNavbar(e)}
     const id = e.currentTarget.getAttribute('href');
     jump(id, {
         duration: 1000,
         offset: -58
-      })
+    })
+    return false;
 }
 
 function toggleNavbar (e) {
-    if (window.innerWidth<776) {
+    if(document.querySelector('.nav-links').clientHeight!=60) {
+        e.preventDefault();
+        e.stopPropagation();
         const links = document.querySelector('.nav-links');
         links.classList.toggle('show')
     }
+    return false;
 }
